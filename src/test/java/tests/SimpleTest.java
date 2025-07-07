2ae4d907d88e70a63e3381955f0ff8545888f75e
ahmed <sdiria673@gmail.com>
2025-07-07 08:52:55 +0100

    edit setHeadless

--- src/test/java/tests/SimpleTest.java
+++ src/test/java/tests/SimpleTest.java
@@ -6,7 +6,7 @@ public class SimpleTest {
 
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             page.navigate("https://playwright.dev/");
             String title = page.title();
